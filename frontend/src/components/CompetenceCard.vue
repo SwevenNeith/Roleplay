8e8ba4bd2edcd7668823c78eaf016c2867393bfe
Camille <camille.four@efrei.net>
2025-07-25 17:18:49 +0200

    Affichage des informations des compétences sur le popover

--- frontend/src/components/CompetenceCard.vue
+++ frontend/src/components/CompetenceCard.vue
@@ -13,19 +13,20 @@
     </p>
     <!-- Popover d'aperçu détaillé -->
     <div v-if="showDetails" class="competence-popover" @mouseenter="showDetails = true" @mouseleave="showDetails = false">
-      <h3>{{ competence.nom }}</h3>
-      <p v-if="competence.type"><strong>Type :</strong> {{ competence.type }}</p>
-      <p v-if="competence.composant && competence.composant.length"><strong>Composant :</strong> {{ competence.composant.join(', ') }}</p>
-      <p v-if="competence.description"><strong>Description :</strong> {{ competence.description }}</p>
+      <!-- <p v-if="competence.type"><strong>Type :</strong> {{ competence.type }}</p> -->
+      <p v-if="competence.systeme"><strong>Système :</strong> {{ competence.systeme }}</p>
+      <p v-if="competence.systeme_value"><strong>Valeur du système :</strong> {{ competence.systeme_value }}</p>
+      <p v-if="competence.voie_slug"><strong>Voie associée :</strong> {{ competence.voie_slug }}</p>
       <p v-if="competence.niveau"><strong>Niveau :</strong> {{ competence.niveau }}</p>
+      <p v-if="competence.composant && competence.composant.length"><strong>Composant :</strong> {{ competence.composant.join(', ') }}</p>
+      <p v-if="competence.portee"><strong>Portée :</strong> {{ competence.portee }}</p>
+      <p v-if="competence.zone"><strong>Zone d'effet :</strong> {{ competence.zone }}</p>
+      <p v-if="competence.temps_incantation"><strong>Temps d'incantation :</strong> {{ competence.temps_incantation }}</p>
+      <p v-if="competence.duree"><strong>Durée :</strong> {{ competence.duree }}</p>
       <p v-if="competence.degats"><strong>Dégâts :</strong> {{ competence.degats }}</p>
       <p v-if="competence.allie"><strong>Effets sur les alliés :</strong> {{ competence.allie }}</p>
       <p v-if="competence.ennemi"><strong>Effets sur les ennemis :</strong> {{ competence.ennemi }}</p>
       <p v-if="competence.sauvegarde"><strong>Sauvegarde :</strong> {{ competence.sauvegarde }}</p>
-      <p v-if="competence.portee"><strong>Portée :</strong> {{ competence.portee }}</p>
-      <p v-if="competence.duree"><strong>Durée :</strong> {{ competence.duree }}</p>
-      <p v-if="competence.voie_slug"><strong>Voie associée :</strong> {{ competence.voie_slug }}</p>
-      <p v-if="competence.theme && competence.theme.length"><strong>Thèmes :</strong> {{ competence.theme.join(', ') }}</p>
     </div>
   </div>
 </template>
